cce0fe8e2f42b8907c9f78c4507deb3cbe1c9571
username10151 <130069436+username10151@users.noreply.github.com>
2023-09-27 17:23:55 -0400

    added mysqlclient, added booking and menu model to restaurant app

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'reservations',
+        'HOST': 'localhost',
+        'USER': 'admindjango',
+        'PASSWORD': 'employee@123!',
+        'PORT': '3306',
     }
 }
 
--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+
+class Bookings(models.Model):
+    name = models.CharField(max_length=255)
+    numberOfGuests = models.IntegerField()
+    bookingDate = models.DateField()
