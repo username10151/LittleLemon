9a735683c806d8e099d35b45d8bff709d08f070a
username10151 <130069436+username10151@users.noreply.github.com>
2023-10-04 16:17:09 -0400

    added single menu item view to /api/menu/<int:pk> using APIView - might have to change to viewset in future

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import menuView, bookingView
+from .views import menuView, bookingView, SingleMenuItemView
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('menu/', menuView.as_view(), name='menu'),
+    path('menu/<int:pk>', SingleMenuItemView.as_view(), name='singleMenuItem'),
     path('booking/', bookingView.as_view(), name='booking'),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Menu, Bookings
 from .serializers import menuSerializer, bookingsSerializer
 
@@ -32,3 +33,31 @@ class menuView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response({'status':'success', 'data': serializer.data})
+
+class SingleMenuItemView(APIView):
+    def get(self, request, pk):
+        try:
+            menu_item = Menu.objects.get(pk=pk)
+            serializer = menuSerializer(menu_item)
+            return Response(serializer.data)
+        except Menu.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, pk):
+        try:
+            menu_item = Menu.objects.get(pk=pk)
+            serializer = menuSerializer(menu_item, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Menu.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def delete(self, request, pk):
+        try:
+            menu_item = Menu.objects.get(pk=pk)
+            menu_item.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Menu.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
